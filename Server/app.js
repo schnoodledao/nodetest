8fd8fc36d9478c30c4aa4363bf11c05dd5936375
Jason Payne <github@x-treem.net>
2022-04-28 00:46:19 +0200

    Secret key support

--- Server/app.js
+++ Server/app.js
@@ -1,11 +1,20 @@
 'use strict'
 
+const { SecretClient } = require("@azure/keyvault-secrets");
+const { DefaultAzureCredential } = require("@azure/identity");
 const fastify = require('fastify')
 const foo = require('./foo/foo.json');
 
+const credential = new DefaultAzureCredential();
+const url = "https://" + process.env.KEY_VAULT_NAME + ".vault.azure.net";
+const client = new SecretClient(url, credential);
+
 function build(opts = {}) {
   const app = fastify(opts);
-  app.get('/Alive', async (request, reply) => sendReply(reply, {status: 'ok', foo}));
+  app.get('/Alive', async (request, reply) => {
+    const secret = await client.getSecret('bridgePrivateKey');
+    return sendReply(reply, {status: 'ok', foo, secret})
+  });
   return app;
 }
 
